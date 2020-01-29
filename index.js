f9d51385a47b893e846e7ed2b75cc95efdb5665f
Kumo <h1710169@nushigh.edu.sg>
2020-01-29 10:18:01 +0800

    memories yay

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const JSDOM = require('jsdom').JSDOM;
 const srcset = require('srcset');
 
 const chapter = require('./chapter.js');
+const memory = require('./memory.js');
 
 const SKIN_PATH = 'images/skins/${id}/';
 const SKIN_NAME_PATH = '${name}/';
@@ -18,6 +19,7 @@ let SHIPS = require("./ships.json");
 let EQUIPMENTS = require("./equipments.internal.json");
 let EQUIPMENTS_PUBLIC = require("./equipments.json");
 let CHAPTERS = require("./chapters.json");
+let MEMORIES = require("./memories.json");
 let SHIPS_INTERNAL = require("./ships.internal.json");
 let VERSION_INFO = require("./version-info.json");
 let IMAGE_PROGRESS = require("./image-progress.json");
@@ -41,6 +43,7 @@ module.exports = {
     refreshShipImages: refreshShipImages,
     refreshEQImages: refreshEQImages,
     refreshChapter: refreshChapter,
+    refreshMemory: refreshMemory,
     removeShip: removeShip
 }
 
@@ -208,6 +211,33 @@ async function refreshChapter(online) {
         fs.writeFileSync('./chapters.json', JSON.stringify(CHAPTERS, null, '\t'));
         console.log("\nDone");
     }
+}
+
+async function refreshMemory(online) {
+    let data;
+    console.log("Recalling Memories");
+    if (!fs.existsSync('./web/memories/index.html') || online) fs.writeFileSync('./web/memories/index.html', data = await fetch("https://azurlane.koumakan.jp/Memories"));
+    else data = fs.readFileSync('./web/memories/index.html', 'utf8');
+    for (let galleryPack of new JSDOM(data).window.document.querySelectorAll(".gallery.mw-gallery-packed")) {
+        let category = camelize(galleryPack.previousElementSibling.textContent.trim());
+        for (let galleryBox of galleryPack.children) {
+            let icon = "https://azurlane.koumakan.jp" + galleryThumbnailUrlToActualUrl(galleryBox.querySelector('.thumb img').src);
+            let name = galleryBox.querySelector(".gallerytext a").textContent;
+            let simple_name = camelize(name.replace(/[^\w\s]/g, ''));
+            let url = "https://azurlane.koumakan.jp" + galleryBox.querySelector(".gallerytext a").href;
+            process.stdout.write(("Recalling \"" + name + "\"...").padEnd(80, ' '));
+            if (!fs.existsSync('./web/memories/' + simple_name + '.html') || online) fs.writeFileSync('./web/memories/' + simple_name + '.html', data = await fetch(url));
+            else data = fs.readFileSync('./web/memories/' + simple_name + '.html', 'utf8');
+            process.stdout.write(" .");
+            MEMORIES[simple_name] = memory.parseMemory(new JSDOM(data).window.document, name, icon, url);
+            const used = process.memoryUsage().heapUsed / 1024 / 1024;
+            process.stdout.write(` done. ${Math.round(used * 100) / 100}MB used\n`);
+            fs.writeFileSync('./memories.json', JSON.stringify(MEMORIES, null, '\t'));
+            if (used > 1000) await timeout(1000);
+        }
+    }
+    fs.writeFileSync('./memories.json', JSON.stringify(MEMORIES, null, '\t'));
+    console.log("\nDone");
 }
 
 function publishShips() {
