7cab6b5765201d8c35b0189c03572440f6315430
misuti <h1710169@nushigh.edu.sg>
2020-06-16 08:23:37 +0800

    lmaox2

--- reset.js
+++ reset.js
@@ -4,7 +4,9 @@ const clearFiles = (path) => fs.readdirSync(path).forEach((file, index) => {
     if (fs.lstatSync(curPath).isDirectory()) clearFiles(curPath);
     else fs.unlinkSync(curPath);
 });
-
+if (!fs.existsSync("./web")) fs.mkdirSync("./web");
+if (!fs.existsSync("./web/ships")) fs.mkdirSync("./web/ships");
+if (!fs.existsSync("./web/ships.gallery")) fs.mkdirSync("./web/ships.gallery");
 clearFiles("./web");
 fs.writeFileSync("ships.json", "{}");
 fs.writeFileSync("ships.internal.json", "{}");
