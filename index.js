d8ee66ed9e269a9b9969ea427baa710264114fc5
misuti <h1710169@nushigh.edu.sg>
2020-07-05 13:29:30 +0800

    lol

--- index.js
+++ index.js
@@ -449,7 +449,7 @@ function parseShip(name, body) {
         ship.rarity = "Unreleased";
         return ship;
     }
-    const misc_selectors = [2, 3, 4, 5, 6].map(i => doc.querySelector(`.nomobile:nth-child(1) > .wikitable tr:nth-child(${i}) > td:nth-child(2) > a`));
+    const misc_selectors = [2, 3, 4, 5, 6].map(i => doc.querySelector(`.nomobile:nth-child(1) tr:nth-child(${i}) a`));
     process.stdout.write(ship.names.en);
     ship.thumbnail = "https://azurlane.koumakan.jp" + doc.getElementsByTagName("img")[0].getAttribute("src");
     ship.rarity = doc.querySelector("div:nth-child(3) > .wikitable td img").parentNode.title;
@@ -461,16 +461,16 @@ function parseShip(name, body) {
     ship.stats = parseStats(doc);
     ship.slots = {};
     for (let i = 0; i < 3; i++) ship.slots[i + 1] = parseShipEQSlot(doc.querySelector(".nomobile > div > .wikitable tr:nth-child(" + (i + 3) + ")"));
-    let enhanceValues = doc.querySelector(".nomobile:nth-child(5) td:nth-child(1)").childNodes;
-    if (enhanceValues.length < 7) ship.enhanceValue = doc.querySelector(".nomobile:nth-child(5) td:nth-child(1)").textContent.trim();
+    let enhanceValues = doc.querySelector(".nomobile:nth-child(4) td:nth-child(1)").childNodes;
+    if (enhanceValues.length < 7) ship.enhanceValue = doc.querySelector(".nomobile:nth-child(4) td:nth-child(1)").textContent.trim();
     else ship.enhanceValue = {
         firepower: parseInt(enhanceValues[0].textContent.trim()),
         torpedo: parseInt(enhanceValues[2].textContent.trim()),
         aviation: parseInt(enhanceValues[4].textContent.trim()),
         reload: parseInt(enhanceValues[6].textContent.trim())
     };
-    let scrapValues = doc.querySelector(".nomobile:nth-child(5) td:nth-child(2)").childNodes;
-    if (scrapValues.length < 5) ship.scrapValue = doc.querySelector(".nomobile:nth-child(5) td:nth-child(2)").textContent.trim();
+    let scrapValues = doc.querySelector(".nomobile:nth-child(4) td:nth-child(2)").childNodes;
+    if (scrapValues.length < 5) ship.scrapValue = doc.querySelector(".nomobile:nth-child(4) td:nth-child(2)").textContent.trim();
     else ship.scrapValue = {
         coin: parseInt(scrapValues[0].textContent.trim()),
         oil: parseInt(scrapValues[2].textContent.trim()),
@@ -485,7 +485,7 @@ function parseShip(name, body) {
         ship.retrofitId = (3000 + parseInt(ship.id)) + "";
         ship.retrofitProjects = parseRetrofit(doc.getElementById("Retrofit").parentElement.nextElementSibling.nextElementSibling.lastElementChild);
     }
-    let obtainedFrom = parseShipObtainedFrom(doc.querySelector("#Construction tbody"));
+    let obtainedFrom = parseShipObtainedFrom(doc.querySelector("#Construction tbody"), ship);
     ship.construction = obtainedFrom.construction;
     ship.obtainedFrom = obtainedFrom.obtainedFrom;
     ship.misc = {
@@ -706,7 +706,23 @@ function parseGallery(name, body) {
     };
 }
 
-function parseShipObtainedFrom(construction_tbody) {
+function parseShipObtainedFrom(construction_tbody, ship) {
+    if (!construction_tbody) return {
+        construction: {
+            constructionTime: ship.rarity === "Priority" || ship.rarity === "Decisive" ? "Research" : "Cannot Be Constructed",
+            availableIn: {
+                "light": false,
+                "heavy": false,
+                "aviation": false,
+                "limited": false,
+                "exchange": false
+            }
+        },
+        obtainedFrom: {
+            obtainedFrom: undefined,
+            fromMaps: []
+        }
+    };
     let construction_time = construction_tbody.children[1].firstElementChild.textContent.trim();
     let available = {};
     let construction_types = ["light", "heavy", "aviation", "limited", "exchange"];
