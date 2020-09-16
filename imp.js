c4210e0bd4c50ad666d750b50acb90002dfd337f
octo-kumo <h1710169@nushigh.edu.sg>
2020-09-17 01:03:12 +0800

    Data Update

--- imp.js
+++ imp.js
@@ -2,12 +2,12 @@ const azurlane = require("./index.js");
 // const voice = require("./voice_lines.js");
 // const barrage = require("./barrage.js");
 // azurlane.removeShip("Emile Bertin");
-// azurlane.refreshShips(false);
+azurlane.refreshShips(false);
 // azurlane.refreshEquipments(false);
 // azurlane.refreshChapter(false);
 // azurlane.refreshShipImages();
 // azurlane.refreshEQImages();
-azurlane.publishShips();
+// azurlane.publishShips();
 // azurlane.publishEQ();
 // azurlane.refreshMemory();
 
