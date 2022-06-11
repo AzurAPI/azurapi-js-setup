88ac6fda1238a321fff7570a37761970daf115e5
Yun <h1710169@nushigh.edu.sg>
2022-06-11 17:27:52 +0800

    Update parser.ts

--- src/ships/parser.ts
+++ src/ships/parser.ts
@@ -124,7 +124,7 @@ export async function parseShip(
   url: string
 ): Promise<Ship> {
   const doc = new JSDOM(body).window.document;
-  let tableInfo = parseTable(doc.querySelector(".nomobile table tbody"));
+  let tableInfo = parseTable(doc.querySelector(".mw-parser-output>.nomobile>div>div>.ship-card tbody"));
   let nationality = tableInfo.Faction;
   let referenceShip = findShip(id, name, nationality);
   let ship = new Ship();
@@ -429,7 +429,7 @@ function parseShipEQSlot(slot: Element): Slot {
 }
 
 function parseStats(doc: Document): ShipStats {
-  let table = doc.querySelector(".mw-parser-output>.nomobile>div>div>.ship-card tbody");
+  let table = doc.querySelector(".mw-parser-output>.nomobile>div>div>table tbody");
   let allStats: ShipStats = { baseStats: {}, level100: {}, level120: {} };
   let titles: string[] = Array.from(table.firstElementChild.children)
     .map((e) =>
